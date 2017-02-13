19b9f8dc2f4038a31aadb6d46d23b035c415ca96
Yaroslav Vorontsov <darth.yarius@gmail.com>
2017-02-13 14:16:16 +0300

    Updated to the latest version of Xcode

--- AutocompleteView/AutocompleteView.h
+++ AutocompleteView/AutocompleteView.h
@@ -16,7 +16,7 @@ FOUNDATION_EXPORT const unsigned char AutocompleteViewVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <AutocompleteView/PublicHeader.h>
 
-#import <TRAutocompleteItemsSource.h>
-#import <TRAutocompleteViewConfig.h>
-#import <TRAutocompletionCellFactory.h>
-#import <TRAutocompleteView.h>
+#import "TRAutocompleteItemsSource.h"
+#import "TRAutocompleteViewConfig.h"
+#import "TRAutocompletionCellFactory.h"
+#import "TRAutocompleteView.h"
